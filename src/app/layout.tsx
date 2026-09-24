3ff741922461ea7f2df66ab24c1ce9fa7f5ef45e
jooseacostaa <joseacostanaranjo@hotmail.com>
2026-09-24 15:42:23 +0100

    ✨ Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 
+import { Analytics } from "@vercel/analytics/next";
 import PageTransition from "@/components/layout/PageTransition";
 
 import "./globals.css";
@@ -18,6 +19,7 @@ export default function RootLayout({
         <html lang="es">
             <body>
                 <PageTransition>{children}</PageTransition>
+                <Analytics />
             </body>
         </html>
     );
